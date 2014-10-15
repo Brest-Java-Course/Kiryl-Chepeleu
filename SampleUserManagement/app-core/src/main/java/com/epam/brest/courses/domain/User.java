3d9617094b226a27dfe46ae1064d281890d940d4
me.ieiunium@gmail.com <me.ieiunium@gmail.com>
2014-10-15 21:49:30 +0300

    second commit

---
+++ SampleUserManagement/app-core/src/main/java/com/epam/brest/courses/domain/User.java
@@ -0,0 +1,25 @@
+package com.epam.brest.courses;
+
+public class User{
+	private long userId;
+	private String login;
+	private String userName;
+	public long getUserId(){
+		return userId;
+	}
+	public String getLogin(){
+		return login;
+	}
+	public String getUserName(){
+		return userName;
+	}
+	public void setUserId(long userId){
+		this.userId=userId;
+	}
+	public void setLogin(String login){
+		this.login=login;
+	}
+	public void setUserId(String userName){
+		this.userName=userName;
+	}
+}
