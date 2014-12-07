6410b2e85ed0ce22dcc121aa6b1728b32b4c6398
Kirill <kirill.kirill1939@gmail.com>
2014-12-07 18:47:43 +0300

    stats

--- HomeTask/app-web/src/main/java/com/epam/brest/courses/web/HelloController.java
+++ HomeTask/app-web/src/main/java/com/epam/brest/courses/web/HelloController.java
@@ -1,19 +1,29 @@
 package com.epam.brest.courses.web;
 
+import com.epam.brest.courses.service.CourseService;
+import com.epam.brest.courses.service.LecturerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.ModelMap;
 
 @Controller
-@RequestMapping("/hello")
 public class HelloController{
+    @Autowired
+    LecturerService lecturerService;
+    @Autowired
+    CourseService courseService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String printHello(ModelMap model) {
-        model.addAttribute("message", "Hello Spring MVC Framework!");
-
+        model.addAttribute("message", "Hello World!");
         return "hello";
     }
-
+    @RequestMapping(value = "/stats",method = RequestMethod.GET)
+    public String printRecords(ModelMap model) {
+        model.addAttribute("lecturersSize", lecturerService.getLecturers().size());
+        model.addAttribute("coursesSize", courseService.getCourses().size());
+        return "stats";
+    }
 }
---
+++ HomeTask/app-web/src/main/webapp/WEB-INF/jsp/stats.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<html>
+<head>
+<title>Hello World</title>
+</head>
+<body>
+   <h2>Lecturers: ${lecturersSize}</h2>
+   <h2>Courses: ${coursesSize}</h2>
+</body>
+</html>
