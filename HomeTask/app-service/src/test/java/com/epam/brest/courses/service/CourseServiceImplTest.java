c0cada99c0807d248dcb82ec2d2f6c37813ff1b9
Kirill <kirill.kirill1939@gmail.com>
2014-12-08 00:31:46 +0300

    course list and lecturers list: add and del

--- HomeTask/app-service/src/test/java/com/epam/brest/courses/service/CourseServiceImplTest.java
+++ HomeTask/app-service/src/test/java/com/epam/brest/courses/service/CourseServiceImplTest.java
@@ -8,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.sql.Date;
 import java.util.List;
 
 import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -27,5 +30,56 @@ public class CourseServiceImplTest {
         List<Course> courses = courseService.getCourses();
         assertNotNull(courses);
         assertFalse(courses.isEmpty());
+    }
+
+    @Test
+    public void addLecturer(){
+        List<Course> courses= courseService.getCourses();
+        int sizeBefore = courses.size();
+        Course course= new Course();
+        course.setCourseName("Name10");
+        course.setStartdate(new Date(2014, 10, 22));
+        course.setListeners(12L);
+        course.setHours(333L);
+        course.setLecturerId(2L);
+        courseService.addCourse(course);
+        courses = courseService.getCourses();
+        assertEquals(sizeBefore, courses.size() - 1);
+    }
+
+    @Test
+    public void removeLecturer(){
+        Course course=new Course(null,"course_name1",3L,111L,13L,new Date(2014,9,26));
+        Long id=courseService.addCourse(course);
+        courseService.removeCourse(id);
+        List<Course> courses = courseService.getCourses();
+        assertNotNull(courses);
+        for(Course i:courses){
+            assertNotEquals(id,i.getCourseId());
+        }
+    }
+
+    @Test
+    public void getCourseByName(){
+        Course course=new Course(null,"course_name2",4L,1111L,13L,new Date(2014,9,26));
+        Long id=courseService.addCourse(course);
+        Course resCourse = courseService.getCourseByName(course.getCourseName());
+        assertNotNull(resCourse);
+        assertEquals(resCourse.getCourseId(),id);
+        assertEquals(resCourse.getCourseName(), course.getCourseName());
+    }
+
+    @Test
+    public void updateCourse(){
+        Course course=new Course(null,"course_name2",4L,1111L,13L,new Date(2014,9,26));
+        Long id=courseService.addCourse(course);
+        Course toUpdatCourse = new Course(id,"course_name3",5L,1111L,13L,new Date(2014,9,22));
+        courseService.updateCourse(toUpdatCourse);
+        Course resCourse = courseService.getCourseById(id);
+        assertEquals(resCourse.getCourseName(),toUpdatCourse.getCourseName());
+        assertEquals(resCourse.getCourseId(),toUpdatCourse.getCourseId());
+        assertEquals(resCourse.getHours(),toUpdatCourse.getHours());
+        assertEquals(resCourse.getLecturerId(),toUpdatCourse.getLecturerId());
+        assertEquals(resCourse.getStartdate(),toUpdatCourse.getStartdate());
     }
 }
--- HomeTask/app-service/src/test/java/com/epam/brest/courses/service/LecturerServiceImplTest.java
+++ HomeTask/app-service/src/test/java/com/epam/brest/courses/service/LecturerServiceImplTest.java
@@ -3,6 +3,7 @@ package com.epam.brest.courses.service;
 import com.epam.brest.courses.dao.LecturerDao;
 import com.epam.brest.courses.domain.Course;
 import com.epam.brest.courses.domain.Lecturer;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -37,5 +39,46 @@ public class LecturerServiceImplTest {
         Long id = lecturerService.addLecturer(lecturer);
         Lecturer resLecturer = lecturerService.getLecturerById(id);
         assertEquals(resLecturer.getLecturerName(),lecturer.getLecturerName());
+    }
+
+    @Test
+    public void removeLecturer() {
+        Lecturer lecturer=new Lecturer(null,"name");
+        Long id=lecturerService.addLecturer(lecturer);
+        lecturerService.removeLecturer(id);
+        List<Lecturer> lecturers = lecturerService.getLecturers();
+        assertNotNull(lecturers);
+        for(Lecturer i:lecturers){
+            assertNotEquals(id,i.getLecturerId());
+        }
+    }
+    @Test
+    public void getLecturerByName(){
+        Lecturer lecturer=new Lecturer(null,"name1");
+        Long id=lecturerService.addLecturer(lecturer);
+        Lecturer resLecturer = lecturerService.getLecturerByName(lecturer.getLecturerName());
+        assertNotNull(resLecturer);
+        Assert.assertEquals(resLecturer.getLecturerId(), id);
+        Assert.assertEquals(resLecturer.getLecturerName(), lecturer.getLecturerName());
+    }
+    @Test
+    public void getLecturerById(){
+        Lecturer lecturer=new Lecturer(null,"name2");
+        Long id=lecturerService.addLecturer(lecturer);
+        Lecturer resLecturer = lecturerService.getLecturerById(id);
+        assertNotNull(resLecturer);
+        Assert.assertEquals(resLecturer.getLecturerId(), id);
+        Assert.assertEquals(resLecturer.getLecturerName(), lecturer.getLecturerName());
+    }
+
+    @Test
+    public void updateLecturer(){
+        Lecturer lecturer=new Lecturer(null,"name3");
+        Long id=lecturerService.addLecturer(lecturer);
+        Lecturer toUpdatlecturer = new Lecturer(id,"name31");
+        lecturerService.updateLecturer(toUpdatlecturer);
+        Lecturer resLecturer = lecturerService.getLecturerById(id);
+        Assert.assertEquals(resLecturer.getLecturerName(), toUpdatlecturer.getLecturerName());
+
     }
 }
